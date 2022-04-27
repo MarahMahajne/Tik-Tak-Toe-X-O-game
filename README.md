83e5185abc5fddf90a4006baa5cdc1c02031de64
MarahMahajne <91371516+MarahMahajne@users.noreply.github.com>
2022-04-28 00:36:26 +0300

    adding files

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Tik-Tak-Toe-X-O-game
 a tik tak toe game with adjustable board size and tow players X and O.
+
+X-0 Game:
+
+main:
+    This function gets a positive integer from the use N, and calls the function manger with the argument N to start
+    the game.
+    if the input N is not valid or less than 3 it keeps asking for another input until availed one is intend.
+
+manger:
+    while the function "finish" is returning false which means that nun of the players won then the game goes on
+    and the manger function makes sure that every player plays on turn:
+        it  starts by initializing  a counter "rounds" to  1.
+        and then if round is odd then its player 1 turn.
+        else if round is even its player 2 turn.
+        and on each round we update "rounds" to "rounds + 1".
+        on each round it calls the function "play" and then prints the board.
+        also in each round we keep track of the number of empty spaces left on the board.
+        and if the number of empty spaces left is 0 and no one won then its a tie we announce the result and end the game.
+    if "finish" function returns true then we check who's turn it was and we announce the winner.
+
+
+createBoard:
+    This function initializes a new board which is a 2D array, and fills the array with "*"
+    and returns the board.
+
+play:
+    This function gets the input of the position from the user, and makes sure that the input is valid,
+    if the input is not valid it keeps asking the player for another input until he inters a valid one.
+    the function also checks if position is not taken otherwise it asks for another position and updates
+    the board with the new move.
+
+finish:
+    This function returns true when one of the player has filled a row ,column, right diagonal or left diagonal
+    returns false if not.
+    in the inner loop it checks each row and each column if the have a mach in all of the cells.
+    in the outer loop we check the right and left diagonal.
+
+printBoard:
+     This function prints the board (2D array).
---
+++ X0Game.java
@@ -0,0 +1,193 @@
+// Marah Mahajne ID: 209107234
+
+import java.util.Scanner;
+
+public class X0Game {
+
+    public static void manger(int N) {
+        boolean player1 = true;
+        boolean player2 = false;
+        boolean tie = false;
+        int rounds = 1, emptySpaces = N * N;
+        char[][] board = createBoard(N);
+        printBoard(board);
+        while (!finish(board)) {
+            if (emptySpaces == 0)
+            {
+                tie = true;
+                break;
+            }
+            else if (rounds % 2 == 0) {
+                player2 = true;
+                player1 = false;
+                System.out.print("Player 2 (O) it's your turn, Enter a position: ");
+            } else {
+                player1 = true;
+                player2 = false;
+                System.out.print("Player 1 (X) it's your turn, Enter a position: ");
+            }
+            play(board, player1,  player2);
+            printBoard(board);
+            emptySpaces --;
+            rounds++;
+        }
+        if (tie) {
+            System.out.print("It's a tie!");
+        } else if (player1) {
+            System.out.print("Congratulations player 1,  You Won!");
+        } else {
+            System.out.print("Congratulations player 2,  You Won!");
+        }
+    }
+
+
+    public static char[][] createBoard(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = '*';
+            }
+        }
+        return board;
+    }
+
+
+    public static void play(char[][] board, boolean player1, boolean player2) {
+        boolean flag = false;
+        int x = -1, y = -1, count = 0;
+        String input;
+        String[] arr;
+        Scanner sc = new Scanner(System.in);
+        while (!flag) {
+            try {
+                count = 0;
+                input = sc.next();
+                for (int i = 0; i < input.length(); i++) {
+                    if (input.charAt(i) == ',') {
+                        count++;
+                    }
+                }
+                arr = input.split(",");
+                if (arr.length == 2 && count == 1) {
+                    x = Integer.parseInt(arr[0]);
+                    y = Integer.parseInt(arr[1]);
+                    flag = true;
+                    if ( x > board.length || y > board.length || x < 0 || y < 0) {
+                        System.out.print("The position you entered is out of range, Enter another position: ");
+                        flag = false;
+                    }
+                } else {
+                    System.out.print("Invalid Input!, enter a valid position (x, y):");
+                    flag = false;
+                }
+            } catch (Exception e) {
+                flag = false;
+                System.out.print("invalid position! enter the number of the raw, column: ");
+            }
+        }
+        x--;
+        y--;
+        // Taken position
+        while (board[x][y] != '*') {
+            System.out.print("This placement is taken, choose another one!: ");
+            input = sc.next();
+            arr = input.split(",");
+            x = Integer.parseInt(arr[0]);
+            y = Integer.parseInt(arr[1]);
+            x--;
+            y--;
+        }
+        if (player1)
+            board[x][y] = 'X';
+        if (player2)
+            board[x][y] = 'O';
+    }
+
+    public static boolean finish(char[][] board) {
+        int cRow = 0, cColumn = 0, cDiagonal = 0, cLeftDiagonal = 0;
+        int n = board.length;
+        char savedR = '*', savedL = '*';
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                //ROW
+                if (board[i][j] == 'X' || board[i][j] == 'O') {
+                    if (j + 1 < board.length) {
+                        if (board[i][j] == board[i][j + 1]) {
+                            cRow++;
+                        }
+                    }
+                }
+                //Column
+                if (board[j][i] == 'X' || board[j][i] == 'O') {
+                    if (j + 1 < board.length) {
+                        if (board[j][i] == board[j + 1][i]) {
+                            cColumn++;
+                        }
+                    }
+                }
+                //Right Diagonal
+                if (board[i][j] == 'X' || board[i][j] == 'O') {
+                    if ( j == i) {
+                        if (savedR == '*') {
+                            savedR = board[i][j];
+                        } else if (savedR == board[i][j]) {
+                            cDiagonal++;
+                        }
+                    }
+                }
+                //left Diagonal
+                if (board[i][j] == 'X' || board[i][j] == 'O') {
+                    if (i + j == board.length - 1) {
+                        if (savedL == '*') {
+                            savedL = board[i][j];
+                        } else if (savedL == board[i][j]) {
+                            cLeftDiagonal++;
+                        }
+                    }
+                }
+            }
+            if (cRow == n - 1 || cColumn == n - 1) {
+                return true;
+            }
+            cRow = 0;
+            cColumn = 0;
+        }
+        if (cLeftDiagonal == n - 1 || cDiagonal == n - 1)
+            return true;
+
+        return false;
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.print("Please enter the size of the board: ");
+        Scanner sc = new Scanner(System.in);
+        boolean flag = false;
+        int N = 0;
+        while(!flag) {
+            try {
+                 N = sc.nextInt();
+                 flag = true;
+                 if (N < 3) {
+                     flag = false;
+                     System.out.print("Invalid input!, Enter a number bigger or equal to 3: ");
+                 }
+            } catch (Exception e) {
+                flag = false;
+                System.out.print("Invalid input!, Enter a number bigger or equal to 3: ");
+                sc.next();
+            }
+        }
+        System.out.print("Game is on !\n");
+        manger(N);
+    }
+}
